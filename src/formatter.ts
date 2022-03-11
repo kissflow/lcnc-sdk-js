0c68d11e7e55a82eb7cf543bb32596eb974a6956
manu2699 <maneeshvijaykar@gmail.com>
2022-03-11 12:29:57 +0530

    ✅ Removed redundant kf in method signature

--- src/formatter.ts
+++ src/formatter.ts
@@ -2,25 +2,25 @@ import { BaseSDK } from "./base";
 import { LISTENER_CMDS } from "./constants";
 
 export class Formatter extends BaseSDK {
-  toKfDate(date: string) {
+  toDate(date: string) {
     return this._postMessageAsync(LISTENER_CMDS.FORMAT_DATE, {
         date,
     });
   }
 
-  toKfDateTime(date: string) {
+  toDateTime(date: string) {
     return this._postMessageAsync(LISTENER_CMDS.FORMAT_DATE_TIME, {
         date,
     });
   }
 
-  toKfNumber(value: string) {
+  toNumber(value: string) {
     return this._postMessageAsync(LISTENER_CMDS.FORMAT_NUMBER, {
         value,
     });
   }
 
-  toKfCurrency(value: string, currencyCode: string) {
+  toCurrency(value: string, currencyCode: string) {
     return this._postMessageAsync(LISTENER_CMDS.FORMAT_CURRENCY, {
         value,
         currencyCode,
