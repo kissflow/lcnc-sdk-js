5e5f3fa806fd6cae15eca152bd2e2b9495391fa5
manu2699 <maneeshvijaykar@gmail.com>
2022-08-23 18:50:17 +0530

    🐛 Component field changes

--- src/component.ts
+++ src/component.ts
@@ -5,30 +5,25 @@ import { ComponentProps } from "./types/internal";
 
 export class Component extends BaseSDK {
 	_id: string;
-	pageId: string;
 	type: string;
 	constructor(props: ComponentProps) {
 		super({});
 		this._id = props.componentId;
 		this.type = "Component";
-		this.pageId = props.pageId;
 	}
 	refresh() {
 		return this._postMessageAsync(LISTENER_CMDS.COMPONENT_REFRESH, {
-			id: this._id,
-			pageId: this.pageId
+			id: this._id
 		});
 	}
 	show() {
 		return this._postMessageAsync(LISTENER_CMDS.COMPONENT_SHOW, {
-			id: this._id,
-			pageId: this.pageId
+			id: this._id
 		});
 	}
 	hide() {
 		return this._postMessageAsync(LISTENER_CMDS.COMPONENT_HIDE, {
-			id: this._id,
-			pageId: this.pageId
+			id: this._id
 		});
 	}
 	watchParams(func: (data: any) => any) {
