79eb4f8bef2a4a840dbb7a92b52b712b621974a2
manu2699 <maneeshvijaykar@gmail.com>
2024-03-28 11:09:43 +0530

    🚨 Update .eslintrc.cjs configuration

--- packages/create-component/template/react/.eslintrc.cjs
+++ packages/create-component/template/react/.eslintrc.cjs
@@ -1,21 +1,6 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	parser: "@typescript-eslint/parser",
+	extends: ["plugin:@typescript-eslint/recommended", "react-app"],
+	plugins: ["@typescript-eslint", "react"],
+	rules: {}
+};
