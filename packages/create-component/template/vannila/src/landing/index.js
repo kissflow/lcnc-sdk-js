7f4045324e804079e03eabec3b9502f4dcc89aa8
manu2699 <maneeshvijaykar@gmail.com>
2024-03-28 22:18:22 +0530

    🔧 Refactor CSS styles and update landing component

---
+++ packages/create-component/template/vannila/src/landing/index.js
@@ -0,0 +1,42 @@
+let kf = window.kf;
+
+function showUserInfo() {
+	kf.client.showInfo(`Hi ${kf.user.Name}!`);
+}
+
+export function defaultLandingComponent() {
+	let userName = kf.user.Name;
+	document.getElementById("root").innerHTML = `
+		<div class='landingHero'>
+			<div class='mainDiv'>
+				<h1>Welcome, ${userName}</h1>
+				<div>
+					<p class='sampletext'>
+						This is a sample custom component pre-loaded with the
+						Kissflow SDK. <br> Edit <code>index.js</code> to make
+						changes.
+					</p>
+					<p class='sampletext'>
+						Click <a
+							href='https://kissflow.github.io/lcnc-sdk-js/'
+							target='_blank'>
+							here
+						</a> to read the SDK documentation.
+					</p>
+				</div>
+				<button id="clickHere">Click me</button>
+				<img src="assets/kf.logo.png" width="120px" class='logo'></img>
+			</div>
+		</div>`;
+	document
+		.getElementById("clickHere")
+		.addEventListener("click", () => showUserInfo());
+}
+
+export function defaultErrorComponent() {
+	document.getElementById("root").innerHTML = `
+		<div class='error'>
+			<h1>Something went wrong</h1>
+			<p>Please use this component within Kissflow platform.</p>
+		</div>`;
+}
