53768843e6535df953abc372b1440d225d2e134b
Satheesh kumar Maharajan <58605123+satheesh66@users.noreply.github.com>
2024-04-18 18:16:18 +0530

    feat : cross origin isolation headers added for template projects

--- packages/create-component/template/react/vite.config.js
+++ packages/create-component/template/react/vite.config.js
@@ -4,16 +4,22 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	base: "",
-	build: {
-		target: "es2022"
-	},
-	server: {
-		https: {
-			cert: path.resolve("./cert/localhost.crt"),
-			key: path.resolve("./cert/localhost.key")
-		},
-		host: "0.0.0.0"
-	}
+  plugins: [react()],
+  base: "",
+  build: {
+    target: "es2022",
+  },
+  server: {
+    https: {
+      cert: path.resolve("./cert/localhost.crt"),
+      key: path.resolve("./cert/localhost.key"),
+    },
+    host: "0.0.0.0",
+    headers: {
+      // Header for cross origin isolation support
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Resource-Policy": "cross-origin",
+    },
+  },
 });
--- packages/create-component/template/vannila/vite.config.js
+++ packages/create-component/template/vannila/vite.config.js
@@ -3,20 +3,26 @@ import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "./",
-	build: {
-		target: "es2022",
-		outDir: "dist",
-		assetsDir: "assets",
-		copyPublicDir: true,
-		minify: false,
-		cssMinify: false
-	},
-	server: {
-		https: {
-			cert: path.resolve("./cert/localhost.crt"),
-			key: path.resolve("./cert/localhost.key")
-		},
-		host: "0.0.0.0"
-	}
+  base: "./",
+  build: {
+    target: "es2022",
+    outDir: "dist",
+    assetsDir: "assets",
+    copyPublicDir: true,
+    minify: false,
+    cssMinify: false,
+  },
+  server: {
+    https: {
+      cert: path.resolve("./cert/localhost.crt"),
+      key: path.resolve("./cert/localhost.key"),
+    },
+    host: "0.0.0.0",
+    headers: {
+      // Header for cross origin isolation support
+      "Cross-Origin-Opener-Policy": "same-origin",
+      "Cross-Origin-Embedder-Policy": "require-corp",
+      "Cross-Origin-Resource-Policy": "cross-origin",
+    },
+  },
 });
