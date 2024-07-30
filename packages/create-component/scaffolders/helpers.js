02060a5b3547d3d4d6e90c1115b9ead350bc12dd
Shibi <shibisuriya.work@gmail.com>
2024-07-30 14:15:50 +0530

    fix: removed c3

--- packages/create-component/scaffolders/helpers.js
+++ packages/create-component/scaffolders/helpers.js
@@ -9,15 +9,4 @@ const rename = (fileName) => {
     return fileName
 }
 
-const getFormFieldProjectTargetKey = (value) => {
-    for (const key in PROJECT_TARGETS) {
-        if (PROJECT_TARGETS[key] === value) {
-            return key
-        }
-    }
-    throw new Error(
-        `The value supplied (${value}, doesn't have an associated key in PROJECT_TARGETS.)`
-    )
-}
-
-export { rename, getFormFieldProjectTargetKey }
+export { rename }
