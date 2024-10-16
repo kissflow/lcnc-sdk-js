023e3ffbcdd93bd674d09671d95737042e2f3c80
Satheesh kumar Maharajan <58605123+satheesh66@users.noreply.github.com>
2024-10-16 11:41:35 +0530

    fix : Url and target updated for other urls

--- docs/src/constants.js
+++ docs/src/constants.js
@@ -2,6 +2,6 @@ export const HOME_URL = "/lcnc-sdk-js";
 
 export const SDK_DOCS_URL = "/lcnc-sdk-js/installation";
 
-export const API_PLATFORM_URL = "https://developers.kissflow.com";
+export const API_PLATFORM_URL = "https://api.kissflow.com";
 
 export const COMMUNITY_URL = "https://community.kissflow.com";
