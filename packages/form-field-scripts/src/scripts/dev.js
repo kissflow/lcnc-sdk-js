70cfaf01750b92d69ec92430db9e5bce6d5ebd9c
Shibi <shibisuriya.work@gmail.com>
2025-01-22 11:34:12 +0530

    fix: temp changes

--- packages/form-field-scripts/src/scripts/dev.js
+++ packages/form-field-scripts/src/scripts/dev.js
@@ -7,6 +7,18 @@ import express from 'express'
 import { WebSocketServer } from 'ws'
 import http from 'http'
 import cors from 'cors'
+import fs from 'fs'
+import path from 'path'
+import https from 'https'
+import { fileURLToPath } from 'url'
+import {
+    createProxyMiddleware,
+    responseInterceptor,
+} from 'http-proxy-middleware'
+import { Readable } from 'stream'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 const port = 9090
 
@@ -32,8 +44,69 @@ const runDevBuild = async () => {
 const startDevServer = async () => {
     const app = express()
     app.use(cors())
-    app.use(express.static(paths.devDist))
+
+    app.use(async (req, res, next) => {
+        console.log('suriya -> ', req.path)
+        if (
+            req.path.startsWith('/customcomponent/formfield') ||
+            req.path.startsWith('/custom_form_field')
+        ) {
+            console.log('shibi -> ', req.path)
+            return next()
+        }
+
+        const url = `https://pegasicdn2.zingworks.com${req.path}`
+
+        try {
+            const externalResponse = await fetch(url)
+            if (!externalResponse.ok) {
+                return res
+                    .status(externalResponse.status)
+                    .send(`Error: ${externalResponse.statusText}`)
+            }
+
+            // Pass the content-type and file as response
+            const contentType = externalResponse.headers.get('content-type')
+            res.set('content-type', contentType)
+
+            // Convert the body to a readable stream and pipe it to the response
+            Readable.from(externalResponse.body).pipe(res)
+        } catch (error) {
+            console.error(`Error fetching URL: ${error.message}`)
+            res.status(500).send('Internal Server Error')
+        }
+    })
+
+    app.use(
+        '/customcomponent/formfield',
+        createProxyMiddleware({
+            target: 'https://development-platform-extension.fecqa.zingworks.com/customcomponent/formfield',
+            changeOrigin: true,
+            selfHandleResponse: true, // res.end() will be called internally by responseInterceptor()
+            on: {
+                proxyRes: responseInterceptor(
+                    async (responseBuffer, proxyRes, req, res) => {
+                        const response = responseBuffer.toString('utf8')
+                        res.removeHeader('content-security-policy')
+                        return response.replaceAll(
+                            'pegasicdn2.zingworks.com',
+                            `127.0.0.1:${port}`
+                        )
+                    }
+                ),
+            },
+        })
+    )
+
+    app.use('/custom_form_field', express.static(paths.devDist))
+
+    const options = {
+        key: fs.readFileSync(path.join(__dirname, './cert/private.key')),
+        cert: fs.readFileSync(path.join(__dirname, './cert/certificate.crt')),
+    }
     const server = http.createServer(app)
+    https.createServer(options, app).listen(port)
+
     const wss = new WebSocketServer({ server })
 
     const clients = new Set()
@@ -46,8 +119,6 @@ const startDevServer = async () => {
     })
 
     await runDevBuild()
-
-    server.listen(port, () => {})
 
     const sourceWatcher = chokidar.watch([paths.appSrc], {
         persistent: true,
